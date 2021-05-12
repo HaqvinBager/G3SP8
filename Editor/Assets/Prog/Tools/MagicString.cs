23f1539103c19e12affdc7904133bace501e5b1c
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-05-12 15:24:15 +0200

    Adding Magic String <3

---
+++ Editor/Assets/Prog/Tools/MagicString.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicString 
+{
+    public static string GeneratedPath { get => "Assets/Generated/"; }
+}
