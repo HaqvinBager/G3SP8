a3379e55274d2d7c5dc44917522838ff2ad71513
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2021-05-26 10:34:11 +0200

    Added PhysicsProps folder, added sound to a PhysicsProp. Added VFXExport script

--- Editor/Assets/Prog/Pipeline/Monobehaviours/VFXReference.cs
+++ Editor/Assets/Prog/Pipeline/Monobehaviours/VFXReference.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExportAudioPropSource : MonoBehaviour
+public class VFXReference : MonoBehaviour
 {
+    public string effectName;
+
     // Start is called before the first frame update
     void Start()
     {
