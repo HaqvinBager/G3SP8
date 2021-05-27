7d71cb1e9c92867e9085ddb16d544e1858fce734
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-05-27 16:22:27 +0200

    no message

--- Editor/Assets/AudioClipChooser.cs
+++ Editor/Assets/AudioClipChooser.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AudioClipChooser : MonoBehaviour
 {
-    public EMusic music;
-    public EGroundType groundType;
-    public EAmbience ambience;
-    public EPropAmbience propAmbience;
-    public ESFX esfx;
-    public ESFXCollection sfxCollection;
-    public EUI uiSFX;
-    public EResearcherEventVoiceLine researcherVoiceLine;
-    public EResearcherReactionVoiceLine researcherReactionVoiceLine;
-    public ERobotVoiceLine RobotVoiceLine;
-    public EChannel channel;
+    //public EMusic music;
+    //public EGroundType groundType;
+    //public EAmbience ambience;
+    //public EPropAmbience propAmbience;
+    //public ESFX esfx;
+    //public ESFXCollection sfxCollection;
+    //public EUI uiSFX;
+    //public EResearcherEventVoiceLine researcherVoiceLine;
+    //public EResearcherReactionVoiceLine researcherReactionVoiceLine;
+    //public ERobotVoiceLine RobotVoiceLine;
+    //public EChannel channel;
 };
