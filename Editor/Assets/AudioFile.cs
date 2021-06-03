1dece37a17b046935064debe82bf7d935c111655
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-06-03 09:36:18 +0200

    unity safe mode FIX

---
+++ Editor/Assets/AudioFile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public enum EMusic { Count };
+public enum EAmbience { Basement1, Basement2, Count };
+public enum EPropAmbience { GrandfatherClock, TVStatic, ElectricalCabinet, Boiler, Refrigerator, Toilet, PhoneCalling, PhoneDead, Count };
+public enum ESFX { GravityGlovePullBuildup, GravityGlovePullHit, GravityGlovePush, GravityGlovePullRelease, Jump, EnemyHit, SwitchPress, PickupGravityGlove, PickupHeal, EnemyAttack, CardboardBox, MovePainting, DoorOpen, Count };
+public enum ESFXCollection { StepWood, StepWoodFast, StepCarpet, StepCarpetFast, StepConcrete, StepConcreteFast, Count };
+public enum EUI { ButtonClick, PlayClick, Count };
+public enum EResearcherEventVoiceLine { V1, V2, BootUp, Intro, Line1, Line2, Line3, Line4, Line5, Line6, Line7, Line8, Line9, Line10, Line11, Line12, Line13, Line14, Line15, Line16, Line17, Line18, Line19, Line20, Line21, Line22, Line23, Line24, Line25, Line26, Line27, Outro1, Outro2, Outro3, Outro4, Outro5, Outro6, Outro7, Count };
+public enum EResearcherReactionVoiceLine { ResearcherReactionExplosives, Count };
+public enum EEnemyVoiceLine { EnemyDamagePlayer, EnemyHeardNoise, EnemyLostPlayer, EnemyBackToPatrol, EnemyPatrol, EnemyFoundPlayer, EnemyChasing, Count };
+public enum EChannel { Music, Ambience, SFX, UI, ResearcherVOX, RobotVOX, DynamicChannel1, DynamicChannel2, DynamicChannel3, Count };
+public enum EGroundType { Concrete, AirVent, Count };
+
+
+
+ public class AudioClipGenerated : MonoBehaviour { 
+}
