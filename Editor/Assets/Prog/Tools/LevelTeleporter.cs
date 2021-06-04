eb5f336a4e067535bab38d5009bc411a1659a699
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2021-06-04 16:21:12 +0200

    Changed order of ELevelName enum

--- Editor/Assets/Prog/Tools/LevelTeleporter.cs
+++ Editor/Assets/Prog/Tools/LevelTeleporter.cs
@@ -7,13 +7,13 @@ using UnityEngine;
 public enum ELevelName
 {
     Cottage_1,
-    Cottage_2,
     Basement_1_1_A,
     Basement_1_1_B,
+    Basement_2,
     Basement_1_2_A,
     Basement_1_2_B,
+    Cottage_2,
     Basement_1_3,
-    Basement_2,
     None
 }
 
