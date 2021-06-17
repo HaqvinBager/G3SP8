61da1003df396b368c311c93e95febc2d784cd4a
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2021-06-17 13:39:31 +0200

    Added creaking sounds, tried fixing bug in AudioActivation

--- IronWrought/Source/Game/AudioActivation.cpp
+++ IronWrought/Source/Game/AudioActivation.cpp
@@ -51,13 +51,15 @@ void CAudioActivation::Update()
 		ToggleHasBeenDelayed();
 		myTime -= mySettings.myDelay;
 	}
+	
+	if (myIs3D)
+		Complete(!myAudioChannel->IsPlaying());
 
 	if (myIsInteracted && myIs3D)
 	{
 		const Matrix& matrix = GameObject().myTransform->GetWorldMatrix();
 		myAudioChannel->Set3DAttributes(matrix.Translation());
 		myAudioChannel->Set3DConeAttributes(matrix.Forward(), mySettings.myStartAttenuationAngle, mySettings.myMaxAttenuationAngle, mySettings.myMinimumVolume);
-		Complete(!myAudioChannel->IsPlaying());
 	}
 }
 
@@ -67,9 +69,22 @@ void CAudioActivation::OnActivation()
 	myIsInteracted = true;
 
 	if (myIs3D)
-		CMainSingleton::PostMaster().SendLate({ EMessageType::PlayDynamicAudioSource, &my3DPlayMessage });
+		CMainSingleton::PostMaster().Send({ EMessageType::PlayDynamicAudioSource, &my3DPlayMessage });
 	else
-		CMainSingleton::PostMaster().SendLate({EMessageType::PlaySFX, &my2DPlayMessage});
+		CMainSingleton::PostMaster().Send({EMessageType::PlaySFX, &my2DPlayMessage});
+
+	// Open
+	if (my3DPlayMessage.mySoundIndex == 35)
+	{
+		my3DPlayMessage.mySoundIndex = 12;
+		my2DPlayMessage = 12;
+	}
+	// Close
+	else if (my3DPlayMessage.mySoundIndex == 12)
+	{
+		my3DPlayMessage.mySoundIndex = 35;
+		my2DPlayMessage = 35;
+	}
 }
 
 void CAudioActivation::OnDisable()
